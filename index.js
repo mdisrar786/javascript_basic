2127785926be0c987eacf4abbc68e8bd9e77799b
Md israr Ahmed <mdisrar164@gmail.com>
2023-12-08 04:45:48 +0530

    closure function

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const apply = document.addEventListener("click",()=>{
-    console.log("button Clicked")
-})
+function one(){
+    let a = 10;
+    function two(){
+        let b = 20
+        function three(){
+            let c = 50;
+            console.log(a)
+            console.log(b)
+        }
+        three()
+    }
+    two()
+}
+one()
